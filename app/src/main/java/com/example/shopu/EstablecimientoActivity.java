320f70187d4cf5c831ecd225e9c1134317753894
Ang-m4 <giraldoma@javeriana.edu.co>
2022-03-09 15:56:42 -0500

    Establishment Activity Created

---
+++ app/src/main/java/com/example/shopu/EstablecimientoActivity.java
@@ -0,0 +1,14 @@
+package com.example.shopu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstablecimientoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_establecimiento);
+    }
+}
