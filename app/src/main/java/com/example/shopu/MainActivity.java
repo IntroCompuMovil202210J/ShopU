5125e9d6d42104ee1b34078db2889f83d24d6195
Felipe-Ariza <arizaluis@javeriana.edu.co>
2022-03-09 21:24:57 -0500

    added main page

--- app/src/main/java/com/example/shopu/MainActivity.java
+++ app/src/main/java/com/example/shopu/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.shopu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
     }
 }
