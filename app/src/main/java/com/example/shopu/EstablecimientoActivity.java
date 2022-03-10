3ea05e17bfcf3527cad6202aa314069e5d7c6333
Ang-m4 <giraldoma@javeriana.edu.co>
2022-03-10 06:20:17 -0500

    First installment finished

--- app/src/main/java/com/example/shopu/EstablecimientoActivity.java
+++ app/src/main/java/com/example/shopu/EstablecimientoActivity.java
@@ -2,13 +2,32 @@ package com.example.shopu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class EstablecimientoActivity extends AppCompatActivity {
 
+    ImageButton ibnVerRuta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_establecimiento);
+
+        ibnVerRuta = findViewById(R.id.ibnVerRuta);
+
+        ibnVerRuta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent log = new Intent(view.getContext(),SeguimientoActivity.class);
+                startActivity(log);
+
+            }
+        });
     }
 }
