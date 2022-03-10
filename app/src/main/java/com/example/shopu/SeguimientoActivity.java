f3ae78f3a53a57f39889e4f00104b8a057468d41
WilliamRB32 <wrodriguezb53@gmail.com>
2022-03-10 04:12:19 -0500

    created tracking screen

--- app/src/main/java/com/example/shopu/SeguimientoActivity.java
+++ app/src/main/java/com/example/shopu/SeguimientoActivity.java
@@ -2,13 +2,37 @@ package com.example.shopu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SeguimientoActivity extends AppCompatActivity {
+
+    ImageButton ibntEstado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seguimiento);
+
+        ibntEstado = findViewById(R.id.ibtnEstado);
+
+
+
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        ibntEstado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent info = new Intent(view.getContext(),SeguimientoInfoActivity.class);
+                startActivity(info);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/shopu/SeguimientoInfoActivity.java
@@ -0,0 +1,17 @@
+package com.example.shopu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class SeguimientoInfoActivity extends AppCompatActivity {
+    String [] arreglo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seguimiento_info);
+
+
+    }
+}
