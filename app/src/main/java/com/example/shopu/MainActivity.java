0146cb88f49b11f64e355d3ecf678e50fda3c174
Felipe-Ariza <arizaluis@javeriana.edu.co>
2022-03-09 22:28:43 -0500

    added login page

--- app/src/main/java/com/example/shopu/MainActivity.java
+++ app/src/main/java/com/example/shopu/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.shopu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent log = new Intent(view.getContext(),LogScreenActivity.class);
+                startActivity(log);
+            }
+        });
     }
 }
