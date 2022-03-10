97c26f28ecff3b4cf8ae9a91ed985d414cb90353
Felipe-Ariza <arizaluis@javeriana.edu.co>
2022-03-10 00:28:40 -0500

    added sigin screen and updated some resources

--- app/src/main/java/com/example/shopu/MainActivity.java
+++ app/src/main/java/com/example/shopu/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnLogin, btnRegister;
+    Button btnLogin, btnSignin;
 
 
     @Override
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogin = findViewById(R.id.btnLogin);
-        btnRegister = findViewById(R.id.btnRegister);
+        btnSignin = findViewById(R.id.btnSignin);
 
     }
 
@@ -31,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent log = new Intent(view.getContext(),LogScreenActivity.class);
                 startActivity(log);
+            }
+        });
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reg = new Intent(view.getContext(),RegisterScreenActivity.class);
+                startActivity(reg);
             }
         });
     }
