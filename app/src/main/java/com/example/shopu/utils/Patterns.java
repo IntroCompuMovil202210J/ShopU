75e104f4e4441fd2e5222a3965163d71c35e482f
David Ramirez <ramirezmonroyd@gmail.com>
2022-05-03 14:48:24 -0500

    Firebase user registration finished

---
+++ app/src/main/java/com/example/shopu/utils/Patterns.java
@@ -0,0 +1,6 @@
+package com.example.shopu.utils;
+
+public class Patterns {
+    public static final String EMAIL_PATTERN = "^[a-z0-9]+(?!.*(?:\\+{2,}|\\-{2,}|\\.{2,}))(?:[\\.+\\-]{0,1}[a-z0-9])*@javeriana\\.edu\\.co$";
+    public static final String NAME_PATTERN = "^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$";
+}
