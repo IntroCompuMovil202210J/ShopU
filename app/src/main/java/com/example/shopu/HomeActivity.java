c009d75a2aa33e92f341577e1876eb5e88558d52
Ang-m4 <giraldoma@javeriana.edu.co>
2022-05-04 01:28:07 -0500

    Establishment adapter created but not implemented

--- app/src/main/java/com/example/shopu/HomeActivity.java
+++ app/src/main/java/com/example/shopu/HomeActivity.java
@@ -6,22 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import com.example.shopu.adapters.EstablishMentAdapter;
+import com.example.shopu.model.Establishment;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
-    Button btnEstablecimiento1;
+
+    ListView gvwEstablishments;
+    EstablishMentAdapter estAdapter;
+    ArrayList<Establishment> establishments;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        gvwEstablishments = findViewById(R.id.gvwEstablishments);
+        System.out.println("ACAA1");
+        establishments = new ArrayList<Establishment>();
+        System.out.println("ACAA2");
+        estAdapter = new EstablishMentAdapter(this,establishments);
+        System.out.println("ACAA3");
+
+        if(estAdapter != null){
+            gvwEstablishments.setAdapter(estAdapter);
+        }
+
+        System.out.println("ACAA4");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        btnEstablecimiento1 = findViewById(R.id.btnEstablecimiento1);
-
-        btnEstablecimiento1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(v.getContext(), EstablecimientoActivity.class);
-                startActivity(i);
-            }
-        });
     }
+
+
 }
