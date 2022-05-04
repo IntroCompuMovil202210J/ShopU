78a8deb704c279f23d39b640d0d75e27208ab7bd
Andres <giraldoma@javeriana.edu.co>
2022-05-04 17:52:53 -0500

    Added establishments to gridView

--- app/src/main/java/com/example/shopu/HomeActivity.java
+++ app/src/main/java/com/example/shopu/HomeActivity.java
@@ -2,43 +2,58 @@ package com.example.shopu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ListView;
 
-import com.example.shopu.adapters.EstablishMentAdapter;
+import com.example.shopu.adapters.EstablishmentAdapter;
 import com.example.shopu.model.Establishment;
 
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ListView gvwEstablishments;
-    EstablishMentAdapter estAdapter;
+    GridView gvwEstablishments;
+    EstablishmentAdapter estAdapter;
     ArrayList<Establishment> establishments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        gvwEstablishments = findViewById(R.id.gvwEstablishments);
-        System.out.println("ACAA1");
-        establishments = new ArrayList<Establishment>();
-        System.out.println("ACAA2");
-        estAdapter = new EstablishMentAdapter(this,establishments);
-        System.out.println("ACAA3");
-
-        if(estAdapter != null){
-            gvwEstablishments.setAdapter(estAdapter);
-        }
-
-        System.out.println("ACAA4");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        loadEstablishments();
+        gvwEstablishments = findViewById(R.id.gvwEstablishments);
+        estAdapter = new EstablishmentAdapter(this,establishments);
+        gvwEstablishments.setAdapter(estAdapter);
+
     }
 
+
+    public void loadEstablishments(){
+
+        this.establishments = new ArrayList<>();
+
+        Establishment e1 = new Establishment(); e1.setName("Establecimiento 1");
+        Establishment e2 = new Establishment(); e2.setName("Establecimiento 2");
+        Establishment e3 = new Establishment(); e3.setName("Establecimiento 3");
+        Establishment e4 = new Establishment(); e4.setName("Establecimiento 4");
+        Establishment e5 = new Establishment(); e5.setName("Establecimiento 5");
+        Establishment e6 = new Establishment(); e6.setName("Establecimiento 6");
+
+        establishments.add(e1);
+        establishments.add(e2);
+        establishments.add(e3);
+        establishments.add(e4);
+        establishments.add(e5);
+        establishments.add(e6);
+        establishments.add(e1);
+        establishments.add(e2);
+        establishments.add(e3);
+        establishments.add(e4);
+        establishments.add(e5);
+        establishments.add(e5);
+
+    }
 
 }
--- app/src/main/java/com/example/shopu/adapters/EstablishmentAdapter.java
+++ app/src/main/java/com/example/shopu/adapters/EstablishmentAdapter.java
@@ -12,23 +12,30 @@ import com.example.shopu.R;
 import com.example.shopu.model.Establishment;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class EstablishMentAdapter extends ArrayAdapter<Establishment> {
+public class EstablishmentAdapter extends ArrayAdapter<Establishment> {
 
-    public EstablishMentAdapter(Context context, ArrayList<Establishment> establishments) {
+    List<Establishment> establishments;
+
+    public EstablishmentAdapter(Context context, ArrayList<Establishment> establishments) {
         super(context, 0, establishments);
+
+        this.establishments = establishments;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
-        Establishment establishment = getItem(position);
+
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_establishment, parent, false);
         }
         // Lookup view for data population
         TextView txtName = (TextView) convertView.findViewById(R.id.txtEstTime);
+
+        Establishment establishment = this.establishments.get(position);
         // Populate the data into the template view using the data object
         txtName.setText(establishment.getName());
 
