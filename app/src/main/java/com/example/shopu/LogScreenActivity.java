70bc05adee9585f1d3b032e63416bba1189a3f84
WilliamRB32 <wrodriguezb53@gmail.com>
2022-05-04 21:05:30 -0500

    Created delivery user

--- app/src/main/java/com/example/shopu/LogScreenActivity.java
+++ app/src/main/java/com/example/shopu/LogScreenActivity.java
@@ -1,25 +1,39 @@
 package com.example.shopu;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.shopu.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LogScreenActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     EditText etxtEmail, etxtPassword;
     Button btnIniciarse;
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef;
+    public static final String PATH_USERS="users/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +48,15 @@ public class LogScreenActivity extends AppCompatActivity {
         btnIniciarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signIn(String.valueOf(etxtEmail.getText()), String.valueOf(etxtPassword.getText()));
+                if(etxtEmail.getText().toString().isEmpty() && etxtPassword.getText().toString().isEmpty() ||
+                        (etxtPassword.getText().toString().isEmpty()) || (etxtEmail.getText().toString().isEmpty())){
+                    Toast.makeText(LogScreenActivity.this, "Valores incompletos", Toast.LENGTH_SHORT).show();
+                }else{
+                    signIn(String.valueOf(etxtEmail.getText()), String.valueOf(etxtPassword.getText()));
+
+                }
+
+
             }
         });
     }
@@ -44,6 +66,7 @@ public class LogScreenActivity extends AppCompatActivity {
         super.onStart();
         //TODO Generar sesion para el usuario que ingresa
         updateUI(mAuth.getCurrentUser());
+
     }
 
     private void signIn(String email, String password) {
@@ -63,9 +86,27 @@ public class LogScreenActivity extends AppCompatActivity {
     }
 
     private void updateUI(FirebaseUser user) {
-        if (user != null)
-            startActivity(new Intent(LogScreenActivity.this, HomeActivity.class));
-        else {
+        if (user != null) {
+            myRef = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+
+            myRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()){
+
+                        if(task.getResult().exists()){
+                            User usuario = task.getResult().getValue(User.class);
+
+                            if(usuario.getType().equals("deliveryMan"))
+                                startActivity(new Intent(LogScreenActivity.this,DeliveryHomeActivity.class));
+                            else
+                                startActivity(new Intent(LogScreenActivity.this,HomeActivity.class));
+                        }
+                    }
+                }
+            });
+
+        }else {
             etxtEmail.setText("");
             etxtPassword.setText("");
         }
--- app/src/main/java/com/example/shopu/MainActivity.java
+++ app/src/main/java/com/example/shopu/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.shopu;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.shopu.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.ktx.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnLogin, btnSignin;
+    Button btnLogin, btnSignin, btnDelivery;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    DatabaseReference myRef;
+    public static final String PATH_USERS="users/";
 
 
     @Override
@@ -19,12 +40,34 @@ public class MainActivity extends AppCompatActivity {
 
         btnLogin = findViewById(R.id.btnLogin);
         btnSignin = findViewById(R.id.btnSignin);
-
+        btnDelivery = findViewById(R.id.btnCreateDelivery);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        if (user!=null){
+            myRef = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+
+            myRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()){
+
+                        if(task.getResult().exists()){
+                            User usuario = task.getResult().getValue(User.class);
+
+                            if(usuario.getType().equals("deliveryMan"))
+                                startActivity(new Intent(MainActivity.this,DeliveryHomeActivity.class));
+                            else
+                                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                        }
+                    }
+                }
+            });
+
+        }
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +82,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent reg = new Intent(view.getContext(),RegisterScreenActivity.class);
                 startActivity(reg);
+            }
+        });
+
+        btnDelivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regDelv = new Intent(view.getContext(),RegisterDeliveryScreenActivity.class);
+                startActivity(regDelv);
             }
         });
     }
